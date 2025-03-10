25800b6d9cf23d5a065c6eb9ecbb2160034672fe
ibrahimm7004 <ibrahim.mahmood57@gmail.com>
2025-03-11 00:10:02 +0500

    added proj

--- bank-app/welcomeWindow.py
+++ bank-app/welcomeWindow.py
@@ -19,7 +19,8 @@ def signup():
     signupWindow()
 
 
-welcomeWindow = Tk(className='Swift app - Welcome')  # welcomeWindow is an object of tkinter class
+# welcomeWindow is an object of tkinter class
+welcomeWindow = Tk(className='Swift app - Welcome')
 
 
 def checkered(canvas, line_distance):
@@ -42,7 +43,8 @@ w.pack()
 
 checkered(w, 40)
 
-luc45 = Font(family="Lucida Console", size=25, weight="normal")  # defining a font to use
+luc45 = Font(family="Lucida Console", size=25,
+             weight="normal")  # defining a font to use
 
 welcomeWindow.geometry("400x350")  # setting the dimensions of this window
 
@@ -50,16 +52,19 @@ bankTitle = Label(welcomeWindow, text="Welcome to SWIFT", font=luc45,
                   wraplength=320)  # bankTitle is an object of class Label
 bankTitle.place(x=50, y=50)  # Used to add a widget(Label) to a window
 bankTitle = Label(welcomeWindow, text="Mobile Banking", font=('Lucida Console', 25, 'bold'),
-                  wraplength=300)  # bankTitle is an object of class Label
-bankTitle.place(x=50, y=100)  # Used to add a widget(Label) to a window
+                  wraplength=300)
+bankTitle.place(x=50, y=100)
 
 # Fonts:
 mon25 = Font(family="Montserrat", size=25, weight="bold")
 mon15 = Font(family="Montserrat", size=15, weight="bold")
 
-exitButton = Button(welcomeWindow, background='light green', text="Exit", font=mon15, command=close)
-LoginButton = Button(welcomeWindow, background='linen', text="Login", font=mon25, command=login)
-SignupButton = Button(welcomeWindow, background='RosyBrown1', text="Sign Up", font=mon15,command=signup)
+exitButton = Button(welcomeWindow, background='light green',
+                    text="Exit", font=mon15, command=close)
+LoginButton = Button(welcomeWindow, background='linen',
+                     text="Login", font=mon25, command=login)
+SignupButton = Button(welcomeWindow, background='RosyBrown1',
+                      text="Sign Up", font=mon15, command=signup)
 
 
 def callURLfunc(url):
@@ -67,7 +72,8 @@ def callURLfunc(url):
 
 
 # Create a Label to display the link
-link = Label(welcomeWindow, text="About", font=('Helvetica bold', 15), fg="blue", cursor="hand2", underline=1)
+link = Label(welcomeWindow, text="About", font=(
+    'Helvetica bold', 15), fg="blue", cursor="hand2", underline=1)
 link.place(x=10, y=310)
 link.bind("<Button-1>", lambda r: callURLfunc("https://www.bankalfalah.com/about/"))
 
