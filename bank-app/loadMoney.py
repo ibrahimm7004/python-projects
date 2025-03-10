81c1fa18918300f485b5094aae7c21e08f5970ca
ibrahimm7004 <ibrahim.mahmood57@gmail.com>
2025-03-11 00:17:21 +0500

    added proj

--- bank-app/loadMoney.py
+++ bank-app/loadMoney.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter.font import *
 
+
 def loadMoneyWindow():
     loadMoneyWindow = Tk(className='Swift app - Load Money')
     loadMoneyWindow.geometry("450x350")
@@ -11,11 +12,10 @@ def loadMoneyWindow():
         for x in range(line_distance, canvas_width, line_distance):
             canvas.create_line(x, 0, x, canvas_height, fill="dark green")
         canvas.create_line(600, 0, 600, canvas_height, fill="dark green")
-        # horizontal lines at an interval of "line_distance" pixel
+        # horiz. lines at an interval of "line_distance" pixel
         for y in range(line_distance, canvas_height, line_distance):
             canvas.create_line(0, y, canvas_width, y, fill="dark green")
         canvas.create_line(0, 160, canvas_width, 160, fill="dark green")
-
 
     canvas_width = 500
     canvas_height = 350
@@ -26,9 +26,11 @@ def loadMoneyWindow():
 
     checkered(w, 20)
 
-    userLabel = Label(loadMoneyWindow, text="Your Transaction ID:", background='light yellow',font=com15)
-    userLabel.place(x=110,y=120)
-    userLabel = Label(loadMoneyWindow, text="IBAN: 682734598269 ", background='powder blue',font=com15)
-    userLabel.place(x=110,y=160)
+    userLabel = Label(loadMoneyWindow, text="Your Transaction ID:",
+                      background='light yellow', font=com15)
+    userLabel.place(x=110, y=120)
+    userLabel = Label(loadMoneyWindow, text="IBAN: 682734598269 ",
+                      background='powder blue', font=com15)
+    userLabel.place(x=110, y=160)
 
     loadMoneyWindow.mainloop()
