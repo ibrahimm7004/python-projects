907171610959bd2fff26443d550c661d651913b1
ibrahimm7004 <ibrahim.mahmood57@gmail.com>
2025-03-12 16:22:55 +0500

    adding new files

---
+++ astro-alert/generate_20_rows.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+def generate():
+    df = pd.read_csv("C:/Users/hp/Desktop/uni/sem4/ds_proj/excel_files/predicted_dataset1.csv")
+
+    cols = ["Radius","Volume","Density","Mass","Radiated Energy","Impact Energy"]
+    df = df.sort_values(by=["Impact Energy"])
+    diff = df["Impact Energy"].diff()
+
+    # Select the 20 rows with the largest gaps between consecutive 'Impact Energy' values
+    idx = diff.nlargest(20).index
+    cols = ["Radius","Volume","Density","Mass","Radiated Energy","Impact Energy"]
+    df_new = df.loc[idx, cols]
+
+    # Round off all values in the DataFrame to 1 decimal point
+    df_new = df_new.round(1)
+
+    df_new.to_csv("C:/Users/hp/Desktop/uni/sem4/ds_proj/excel_files/output.csv", index=False)
+    return df_new
+
+
+
+
+
+
+
+
+
